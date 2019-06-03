f3e05881219ebd39aba6d21481fc5711e10e275a
れみろう <remyroez+github@gmail.com>
2019-06-04 08:41:09 +0900

    :sparkles: Larger than Life ルールを実装

--- game/src/LargerThanLife.lua
+++ game/src/LargerThanLife.lua
@@ -15,81 +15,86 @@ function LargerThanLife.newRule()
         count = 0,
         middle = 0,
         survive = { min = 0, max = 0 },
-        birth   = { min = 0, max = 0, },
-        neighborhood = 'moore',
+        birth = { min = 1, max = 1, },
+        neighborhood = 'M',
     }
 end
 
 -- 新規ランダムルール
-function LargerThanLife.newRandomRule(min, max)
+function LargerThanLife.newRandomRule()
+    local smin = random(0, 10)
+    local bmin = random(0, 10)
     return {
         type = 'LargerThanLife',
-        range = 1,
-        count = 0,
-        middle = 0,
-        survive = { min = 0, max = 0 },
-        birth   = { min = 0, max = 0, },
-        neighborhood = 'moore',
+        range = random(1, 10),
+        count = random(0, 100),
+        middle = random(0, 1),
+        survive = { min = smin, max = random(smin, smin * 2) },
+        birth = { min = bmin, max = random(bmin, bmin * 2), },
+        neighborhood = util.randomBool() and 'M' or 'N',
     }
 end
 
 -- ルールを文字列化
 function LargerThanLife.toString(rule)
-    rule = type(rule) == 'table' and rule or {}
+    rule = type(rule) == 'table' and rule or LargerThanLife.newRule()
 
-    local buffer = 'R' .. rule.range
-
-    for i, bool in ipairs(rule.birth) do
-        if bool then
-            buffer = buffer .. tostring(i - 1)
-        end
-    end
-
-    buffer = buffer .. '/S'
-
-    for i, bool in ipairs(rule.survive) do
-        if bool then
-            buffer = buffer .. tostring(i - 1)
-        end
-    end
-
-    buffer = buffer .. '/C' .. rule.count
-
-    return buffer
+    return
+        'R' .. tostring(rule.range) .. ',',
+        'C' .. tostring(rule.count) .. ',',
+        'M' .. tostring(rule.middle) .. ',',
+        'S' .. tostring(rule.survive.min) .. '..' .. tostring(rule.survive.max) .. ',',
+        'B' .. tostring(rule.birth.min) .. '..' .. tostring(rule.birth.max) .. ',',
+        'N' .. rule.neighborhood
 end
 
 -- ルールを文字列化
 function LargerThanLife.toRule(str)
-    str = type(str) == 'string' and str or 'B/S/C'
+    str = type(str) == 'string' and str or ''
 
-    local rule = LargerThanLife.newRule(0)
+    -- 空ルール
+    local rule = LargerThanLife.newRule()
+    rule.range = 0
+    rule.birth.min = 0
+    rule.birth.max = 0
 
-    local target = 'birth'
+    local target = ''
+    local subtarget = 'min'
     for i = 1, string.len(str) do
         local c = string.sub(str, i, i)
-        if c == 'B' then
-            target = 'birth'
-        elseif c == 'S' then
-            target = 'survive'
+        if c == 'R' then
+            target = 'range'
         elseif c == 'C' then
             target = 'count'
-        elseif c == '/' then
-            if target == 'birth' then
-                target = 'survive'
-            elseif target == 'survive' then
-                target = 'count'
-            elseif target == 'count' then
-            else
-            end
+        elseif c == 'M' then
+            target = 'middle'
+        elseif c == 'S' then
+            target = 'survive'
+            subtarget = 'min'
+        elseif c == 'B' then
+            target = 'birth'
+            subtarget = 'min'
+        elseif c == 'N' then
+            target = 'neighborhood'
+        elseif c == ',' then
+            -- 区切り文字（リセット）
+            target = ''
+        elseif c == '.' then
+            subtarget = 'max'
+        elseif target == 'neighborhood' and c == 'M' or c == 'N' then
+            -- 近傍
+            rule.neighborhood = c
+        elseif target == '' then
+            -- ターゲットがないなら、数字を取るべきではない
         else
             local n = tonumber(c)
             if not n then
                 -- 数字じゃない
-            elseif target == 'count' then
-                -- カウント時
-                rule.count = rule.count * 10 + n
+            elseif target == 'survive' or target == 'birth' then
+                -- 生存／誕生
+                rule[target][subtarget] = rule[target][subtarget] * 10 + n
             else
-                rule[target][n + 1] = true
+                rule[target] = rule[target] * 10 + n
             end
         end
     end
@@ -103,11 +108,17 @@ function LargerThanLife.validate(rule)
 
     if type(rule) ~= 'table' then
     elseif rule.type ~= 'LargerThanLife' then
-    elseif type(rule.birth) ~= 'table' then
-    elseif #rule.birth ~= 9 then
-    elseif type(rule.survive) ~= 'table' then
-    elseif #rule.survive ~= 9 then
+    elseif type(rule.range) ~= 'number' then
     elseif type(rule.count) ~= 'number' then
+    elseif type(rule.middle) ~= 'number' then
+    elseif type(rule.survive) ~= 'table' then
+    elseif type(rule.survive.min) ~= 'number' then
+    elseif type(rule.survive.max) ~= 'number' then
+    elseif type(rule.birth) ~= 'table' then
+    elseif type(rule.birth.min) ~= 'number' then
+    elseif type(rule.birth.max) ~= 'number' then
+    elseif type(rule.birth.max) ~= 'number' then
+    elseif rule.neighborhood ~= 'M' and rule.neighborhood ~= 'N' then
     else
         ok = true
     end
